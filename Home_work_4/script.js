72d901ce0f279df524ef48fae1875fcea95780b9
kolesivo <36096601+kolesivo@users.noreply.github.com>
2023-05-21 17:18:28 +0400

    Create script.js

---
+++ Home_work_4/script.js
@@ -0,0 +1,27 @@
+const btn = document.querySelector('.btn');
+const contentNode = document.querySelector('#content');
+
+function useRequest(firstInput, secondInput) {
+    return fetch(`https://picsum.photos/${firstInput}/${secondInput}`)
+        .then(response => response.blob())
+        .then(blob => URL.createObjectURL(blob))
+        .catch(() => {
+            console.log('error')
+            contentNode.innerHTML = '<p> Что-то пошло не так</p>'
+        })
+}
+
+btn.addEventListener('click', async () => {
+    const inputWidth = document.querySelector('#first_input').value;
+    const inputHeight = document.querySelector('#second_input').value;
+    let minSize = 100,
+        maxSize = 300;
+    if ((inputWidth >= minSize && inputWidth <= maxSize) && (inputHeight >= minSize && inputHeight <= maxSize)){
+        const url = await useRequest(inputWidth, inputHeight)
+        contentNode.innerHTML = `
+      <img src='${url}' alt='image'>`
+    } else {
+        contentNode.innerHTML = `
+      <p>Одно из чисел вне диапазона от ${minSize} до ${maxSize}</p>`
+    }
+});
